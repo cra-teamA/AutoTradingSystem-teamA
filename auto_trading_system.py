2d6b9c3cd213eba1ae67d112c115263e065ca192
hwieun <hwieunlee98@gmail.com>
2025-08-04 14:02:51 +0900

    [feature] auto_Trading_system Green

--- auto_trading_system.py
+++ auto_trading_system.py
@@ -1,3 +1,7 @@
+from time import sleep
+
+from driver_mock import MockDriver
+
 class AutoTradingSystem:
     def __init__(self):
         self._stock_brocker = None
@@ -6,21 +10,37 @@ class AutoTradingSystem:
     def selectStockBrocker(self, stock_brocker):
         self._stock_brocker = stock_brocker
 
-    def login(self, id):
-        ...
+    def login(self, id, password):
+        self._stock_brocker.login(id, password)
 
-    def buy(self, code, price, quantity): #refactor : interface에 추가
-        ...
+    def buy(self, stock, price, quantity):
+        self._stock_brocker.buy(stock, price, quantity)
 
-    def sell(self, code, price, quantity): #refactor : interface에 추가
-        ...
+    def sell(self, stock, price, count):
+        self._stock_brocker.sell(stock, price, count)
 
-    def getPrice(self, code):
-        ...
+    def getPrice(self, stock):
+        return self._stock_brocker.get(stock)
 
-    # 핵심 기능
     def getbuyNiceTiming(self, stock, price):
-        ...
+        price1 = self._stock_brocker.get(stock)
+        sleep(200/1000)
+        price2 = self._stock_brocker.get(stock)
+        sleep(200/1000)
+        price3 = self._stock_brocker.get(stock)
+        if not (price1 < price2 < price3):
+            raise Exception()
+        else:
+            count = price // price3
+            return self._stock_brocker.buy(stock, count, price3)
 
-    def sellNiceTiming(self, quantity):
-        ...
+    def sellNiceTiming(self, stock, count):
+        price1 = self._stock_brocker.get(stock)
+        sleep(200/1000)
+        price2 = self._stock_brocker.get(stock)
+        sleep(200/1000)
+        price3 = self._stock_brocker.get(stock)
+        if not(price1 > price2 > price3):
+            raise Exception()
+        else:
+            return self._stock_brocker.sell(stock, count, price3)
--- test_auto_trading_system.py
+++ test_auto_trading_system.py
@@ -9,32 +9,57 @@ PRICE = 100000
 
 
 @pytest.fixture
-def ats_with_mock_driver(mocker):
-    mock_driver = mocker.Mock()
+def ats_with_mock_interface(mocker):
+    mock_interface = mocker.Mock()
     ats = AutoTradingSystem()
-    ats.selectStockBrocker(mock_driver)
-    return ats, mock_driver
+    ats.selectStockBrocker(mock_interface)
+    return ats, mock_interface
 
 
-def test_ats_login(ats_with_mock_driver):
-    ats, driver = ats_with_mock_driver
+def test_ats_login(ats_with_mock_interface):
+    ats, inteface = ats_with_mock_interface
 
     ats.login(ID, PW)
 
-    driver.login.assert_called_once_with(ID, PW)
+    inteface.login.assert_called_once_with(ID, PW)
 
 
-def test_ats_buy(ats_with_mock_driver):
-    ats, driver = ats_with_mock_driver
+def test_ats_buy(ats_with_mock_interface):
+    ats, interface = ats_with_mock_interface
 
     ats.buy(STOCK, PRICE, COUNT)
 
-    driver.buy.assert_called_once_with(STOCK, PRICE, COUNT)
+    interface.buy.assert_called_once_with(STOCK, PRICE, COUNT)
 
 
-def test_ats_sell(ats_with_mock_driver):
-    ats, driver = ats_with_mock_driver
+def test_ats_sell(ats_with_mock_interface):
+    ats, interface = ats_with_mock_interface
 
     ats.sell(STOCK, PRICE, COUNT)
 
-    driver.sell.assert_called_once_with(STOCK, PRICE, COUNT)
+    interface.sell.assert_called_once_with(STOCK, PRICE, COUNT)
+
+def test_get_Price(ats_with_mock_interface):
+    ats, interface = ats_with_mock_interface
+    interface.get.return_value = 1000
+    assert ats.getPrice(STOCK) == 1000
+
+@pytest.mark.parametrize("lst",
+[(10,12,14)]
+ )
+def test_getbuyNiceTiming(ats_with_mock_interface, lst):
+    ats, interface = ats_with_mock_interface
+    interface.get.side_effect = lst
+    ats.getbuyNiceTiming(STOCK, PRICE)
+    assert interface.get.call_count == 3
+    interface.buy.assert_called_once_with(STOCK,PRICE//14,14)
+
+@pytest.mark.parametrize("lst",
+[(10,8,6)]
+ )
+def test_sellNiceTiming(ats_with_mock_interface, lst):
+    ats, interface = ats_with_mock_interface
+    interface.get.side_effect = lst
+    ats.sellNiceTiming(STOCK, COUNT)
+    assert interface.get.call_count == 3
+    interface.sell.assert_called_once_with(STOCK,COUNT,6)
