fdb3954a83ae2818ad7f0351707a71077d3c44b3
soiscow <thgus_so@naver.com>
2025-08-04 13:48:13 +0900

    [refact] add without_login fixture

--- test_driver_interface.py
+++ test_driver_interface.py
@@ -12,31 +12,36 @@ COUNT = 1
 PRICE = 100000
 
 @pytest.fixture
-def driver_interface_data(mocker):
+def driver_interface_data_with_login(mocker):
     driver = mocker.Mock()
-    driverif = StockBrockerDriverInterface(driver)
-    driverif.login(ID, PW)
-    return driverif, driver
+    driver_interface = StockBrockerDriverInterface(driver)
+    driver_interface.login(ID, PW)
+    return driver_interface, driver
+
+@pytest.fixture
+def driver_interface_data_without_login(mocker):
+    driver = mocker.Mock()
+    driver_interface = StockBrockerDriverInterface(driver)
+    return driver_interface, driver
 
 
-def test_interface_creation(driver_interface_data):
-    driver_interface, driver = driver_interface_data
+def test_interface_creation(driver_interface_data_with_login):
+    driver_interface, driver = driver_interface_data_with_login
     assert driver_interface is not None
 
 
-def test_login(capsys, mocker):
-    driver = mocker.Mock()
-    driverif = StockBrockerDriverInterface(driver)
+def test_login(capsys, driver_interface_data_without_login):
+    driver_interface, driver = driver_interface_data_without_login
     try:
-        driverif.login(ID, PW)
+        driver_interface.login(ID, PW)
     except:
         pytest.fail()
     catured = capsys.readouterr()
     assert 'login' in catured.out
     assert driver.login.called == 1
 
-def test_login_fail(capsys, driver_interface_data):
-    driver_interface, driver = driver_interface_data
+def test_login_fail(capsys, driver_interface_data_with_login):
+    driver_interface, driver = driver_interface_data_with_login
     with pytest.raises(Exception):
         driver_interface.login(ID, PW)
 
@@ -45,8 +50,8 @@ def test_login_fail(capsys, driver_interface_data):
 
     assert driver.login.called == 1
 
-def test_get(driver_interface_data):
-    driver_interface, driver = driver_interface_data
+def test_get(driver_interface_data_with_login):
+    driver_interface, driver = driver_interface_data_with_login
 
     driver.get_price.return_value = 1
     ret = driver_interface.get(STOCK)
@@ -54,8 +59,8 @@ def test_get(driver_interface_data):
     assert driver.get_price.called == 1
 
 
-def test_buy(capsys, driver_interface_data):
-    driver_interface, driver = driver_interface_data
+def test_buy(capsys, driver_interface_data_with_login):
+    driver_interface, driver = driver_interface_data_with_login
     driver.buy.return_value =  print('Buy stock')
     try:
         driver_interface.buy(STOCK, COUNT, PRICE)
@@ -66,8 +71,8 @@ def test_buy(capsys, driver_interface_data):
     assert driver.buy.called == 1
 
 
-def test_sell(capsys, driver_interface_data):
-    driver_interface, driver = driver_interface_data
+def test_sell(capsys, driver_interface_data_with_login):
+    driver_interface, driver = driver_interface_data_with_login
     driver.sell.return_value = print('Sell stock')
 
     try:
